48559e47d800b20bcaee9a5c83e16c84f35eadb1
Marco Preciado <mapnzb@umsystem.edu>
2021-08-05 15:34:02 -0700

    DD_Pays Tables

---
+++ DDTT_SqlProj/dbo/Tables/DD_Pays.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[DD_Pays]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Amount] MONEY NULL, 
+    [TimeStamp] DATETIME2 NULL
+)
--- DDTT_SqlProj/dbo/Tables/Expenditures.sql
+++ DDTT_SqlProj/dbo/Tables/Expenditures.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[Expenditures]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Amount] MONEY NOT NULL, 
+    [TimeStamp] DATETIME2 NOT NULL, 
+    [Note] NVARCHAR(150) NULL, 
+    [ExpenseTypeId] INT NOT NULL
 )
