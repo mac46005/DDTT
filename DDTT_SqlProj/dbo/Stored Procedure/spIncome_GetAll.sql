1a6bac22c6066b67c08ef81793d2332d6fb06b5b
Marco Preciado <mac46005@gmail.com>
2021-08-05 17:07:39 -0500

    spIncome_GetAll

---
+++ DDTT_SqlProj/dbo/Stored Procedure/spIncome_GetAll.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spIncome_GetAll]
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT Id,DD_Pay_Id,Tip_Id
+	FROM dbo.Incomes;
+END
---
+++ DDTT_SqlProj/dbo/Stored Procedure/spIncome_GetById.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spIncome_GetById]
+	@Id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT Id,DD_Pay_Id,Tip_Id
+	FROM dbo.Incomes
+	WHERE Id = @Id;
+END
