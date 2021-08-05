d1327c3fc95d06364b206f4dfcdae4518893826c
Marco Preciado <mapnzb@umsystem.edu>
2021-08-05 12:45:17 -0700

    Edited TaxRateSchedule_MJointly_Widow

--- DDTT.UI_Commands.ClassLib/Models/TaxModels/TaxRateSchedule_MJointly_Widow.cs
+++ DDTT.UI_Commands.ClassLib/Models/TaxModels/TaxRateSchedule_MJointly_Widow.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static DDTT.UI_Commands.ClassLib.Models.TaxModels.TaxRateScheduleHelper;
 
 namespace DDTT.UI_Commands.ClassLib.Models.TaxModels
 {
@@ -16,7 +17,36 @@ namespace DDTT.UI_Commands.ClassLib.Models.TaxModels
 
         public decimal TaxAmount(decimal line3)
         {
-            throw new NotImplementedException();
+            decimal amount = 0;
+            if (line3 >= compareToLine3[0] || line3 <= compareToLine3[1])
+            {
+                amount = CalculateTaxAmount(0,0.10M);
+            }
+            else if (line3 >= compareToLine3[1] || line3 <= compareToLine3[2])
+            {
+                amount = CalculateTaxAmount(1990, 0.12M);
+            }
+            else if (line3 >= compareToLine3[2] || line3 <= compareToLine3[3])
+            {
+                amount = CalculateTaxAmount(9328, 0.22M);
+            }
+            else if (line3 >= compareToLine3[3] || line3 <= compareToLine3[4])
+            {
+                amount = CalculateTaxAmount(29502, 0.24M);
+            }
+            else if (line3 >= compareToLine3[4] || line3 <= compareToLine3[5])
+            {
+                amount = CalculateTaxAmount(67206, 0.32M);
+            }
+            else if (line3 >= compareToLine3[5] || line3 <= compareToLine3[6])
+            {
+                amount = CalculateTaxAmount(95686, 0.35M);
+            }
+            else if (line3 >= compareToLine3[6])
+            {
+                amount = CalculateTaxAmount(168993, 0.37M);
+            }
+            return amount;
         }
     }
 }
