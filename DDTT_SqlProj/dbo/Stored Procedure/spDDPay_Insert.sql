eef5f001cf2c9cece634eb6ab8904c30d86c14be
Marco Preciado <mac46005@gmail.com>
2021-08-05 16:03:12 -0500

    spDDPay_Insert

---
+++ DDTT_SqlProj/dbo/Stored Procedure/spDDPay_Insert.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spDDPay_Insert]
+	@Id INT OUTPUT,
+	@Amount MONEY,
+	@TimeStamp DATETIME2
+AS
+BEGIN
+SET NOCOUNT ON;
+INSERT INTO dbo.DD_Pays(Amount,TimeStamp)
+VALUES(@Amount,@TimeStamp)
+SELECT @Id = SCOPE_IDENTITY();
+END
