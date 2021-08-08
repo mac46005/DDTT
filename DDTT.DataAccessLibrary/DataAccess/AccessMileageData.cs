a4dd76eee7e01cf8440449ff773cbdbd6613c6fc
Marco Preciado <mapnzb@umsystem.edu>
2021-08-07 19:40:28 -0700

    AccessMileageData DeleteById

---
+++ DDTT.DataAccessLibrary/DataAccess/AccessMileageData.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.ClassLib.GenericDataAccess;
+using DataAccess.ClassLib.Interface.BasicDataAccess_Interfaces;
+using DDTT.ClassLib.Models.BusinessModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDTT.DataAccessLibrary.DataAccess
+{
+    public class AccessMileageData
+    {
+        public Mileage GetById(int id)
+        {
+            using (ILoadSingleData cnn = new BasicSqlDataAccess())
+            {
+                return cnn.LoadSingleData<Mileage, dynamic>("dbo.spMileage_GetById", "", new { Id = id });
+            }
+        }
+        public List<Mileage> GetAll()
+        {
+            using (ILoadSetData cnn = new BasicSqlDataAccess())
+            {
+                return cnn.LoadSetData<Mileage, dynamic>("dbo.spMileage_GetAll", "", new { });
+            }
+        }
+        public void DeleteById(int id)
+        {
+            using(ISaveSingleData cnn = new BasicSqlDataAccess())
+            {
+                cnn.SaveData<dynamic>("dbo.spMileage_DeleteById", "",);
+            }
+        }
+    }
+}
