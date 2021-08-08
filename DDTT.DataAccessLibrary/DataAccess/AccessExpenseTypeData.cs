bec4e21651af3fb64e72d12ca0b17a1a9ae9e4a3
Marco Preciado <mapnzb@umsystem.edu>
2021-08-07 20:38:34 -0700

    AccessExpenseTypeData GetById

---
+++ DDTT.DataAccessLibrary/DataAccess/AccessExpenseTypeData.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.ClassLib.GenericDataAccess;
+using DataAccess.ClassLib.Interface.BasicDataAccess_Interfaces;
+using DDTT.ClassLib.Models.BusinessModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDTT.DataAccessLibrary.DataAccess
+{
+    public class AccessExpenseTypeData
+    {
+        public ExpenseType GetById(int id)
+        {
+            using (ILoadSingleData cnn = new SqlDataAccess())
+            {
+                return cnn.LoadSingleData<ExpenseType,dynamic>("dbo.spExpenseType_GetById", "", new { Id = id });
+            }
+        }
+    }
+}
