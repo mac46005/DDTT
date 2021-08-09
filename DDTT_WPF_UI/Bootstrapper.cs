5798519526a913f1c61297d3238106f7adec0dc0
Marco Preciado <mapnzb@umsystem.edu>
2021-08-09 01:01:30 -0700

    Configured Bootstrapper

--- DDTT_WPF_UI/Bootstrapper.cs
+++ DDTT_WPF_UI/Bootstrapper.cs
@@ -3,12 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using AutoMapper;
 using Caliburn.Micro;
+using DDTT_WPF_UI.ViewModels;
 
 namespace DDTT_WPF_UI
 {
     internal class Bootstrapper : BootstrapperBase
     {
         private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void Configure()
+        {
+            var configMapper = new MapperConfiguration(cfg => 
+            {
+
+            });
+
+            var mapper = configMapper.CreateMapper();
+            _container.Instance<IMapper>(mapper);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(vmType => _container.RegisterPerRequest(
+                    vmType, vmType.ToString(), vmType));
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
     }
 }
