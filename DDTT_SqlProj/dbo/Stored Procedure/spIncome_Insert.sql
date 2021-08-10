c995aa6eb8068fe3c6f254aef436bd376a3f0643
Marco Preciado <mac46005@gmail.com>
2021-08-10 13:30:15 -0500

    spIncome_Insert

---
+++ DDTT_SqlProj/dbo/Stored Procedure/spIncome_Insert.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spIncome_Insert]
+	@JobTypeId INT,
+	@BasePay MONEY,
+	@Tip MONEY,
+	@TimeStamp DATETIME2
+AS
+BEGIN
+	INSERT INTO dbo.Incomes(JobTypeId,BasePay,Tip,TimeStamp)
+	VALUES(@JobTypeId,@BasePay,@Tip,@TimeStamp);
+END
