0ca759435c3790eb3b49caad060030cafedf21b0
Marco Preciado <mac46005@gmail.com>
2021-08-14 21:05:48 -0500

    Continue on AddMileageViewModel

--- DDTT_WPF_UI/ViewModels/AddMileageViewModel.cs
+++ DDTT_WPF_UI/ViewModels/AddMileageViewModel.cs
@@ -1,13 +1,31 @@
-﻿using Caliburn.Micro;
+using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DDTT.ClassLib.Models.BusinessModels;
+using DDTT.DataAccessLibrary.DataAccess.Interfaces;
+
 
 namespace DDTT_WPF_UI.ViewModels
 {
     internal class AddMileageViewModel : Screen
     {
+      private IGetAll<JobType> _getAllJobTypes;
+      private IInsert<Mileage> _insertMileage;
+      public AddMileageViewModel(IDataAccess<JobType> accessJobTypeData,IDataAccess<Mileage> accessMileageData){
+        _getAllJobTypes = accessJobTypeData;
+        _insertMileage = accessMileageData;
+      }
+      
+      public List<JobType> JobTypeList {get;set;} = new List<JobType>();
+      public JobType SelectedJobType {get;set;}
+      
+      
+      
+      
+      
+      
     }
 }
