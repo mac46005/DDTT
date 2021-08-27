4e73af2dc3d38dd18d1eb3da6a01838b6a8d94c0
Marco Preciado <mac46005@gmail.com>
2021-08-27 11:44:26 -0500

    spMileage_EditById

---
+++ DDTT_SqlProj/dbo/Stored Procedure/spMileage_EditById.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spMileage_EditById]
+	@Id INT,
+	@JobTypeId INT,
+	@Amount MONEY,
+	@TimeStamp DATETIME2(7)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE dbo.Mileages
+	SET 
+	JobTypeId = @JobTypeId,
+	Amount = @Amount,
+	TimeStamp = @TimeStamp
+	WHERE Id = @Id
+END
