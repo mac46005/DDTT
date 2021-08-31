6000064abc9c8b7c23e1209b54507cc1fe96a330
Marco Preciado <mac46005@gmail.com>
2021-08-31 18:09:39 -0500

    line 2 data access ready

--- SelfEmployedTaxEstimator.ClassLibrary/DataAccessModels/Access1040ES_SpecificLines/Line2DataAccess.cs
+++ SelfEmployedTaxEstimator.ClassLibrary/DataAccessModels/Access1040ES_SpecificLines/Line2DataAccess.cs
@@ -7,11 +7,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static DataAccess.ClassLib.DBNameHelper;
 
 namespace SelfEmployedTaxEstimator.ClassLibrary.DataAccessModels.Access1040ES_SpecificLines
 {
     class Line2DataAccess : IDataAccess<Line2>
     {
+        const string line2 = nameof(Line2);
         public void DeleteById(int id)
         {
             using (ISaveSingleData cnn = new SqlDataAccess())
@@ -24,7 +26,7 @@ namespace SelfEmployedTaxEstimator.ClassLibrary.DataAccessModels.Access1040ES_Sp
         {
             using (ISaveSingleData cnn = new SqlDataAccess())
             {
-                cnn.SaveData<dynamic>("", "", new { });
+                cnn.SaveData<dynamic>(dbo_sp() + line2 + Pre_("Update"), "", new { });
             }
         }
 
@@ -40,7 +42,7 @@ namespace SelfEmployedTaxEstimator.ClassLibrary.DataAccessModels.Access1040ES_Sp
         {
             using (ILoadSingleData cnn = new SqlDataAccess())
             {
-                return cnn.LoadSingleData<Line2, dynamic>("", "", new { });
+                return cnn.LoadSingleData<Line2, dynamic>(dbo_sp() + line2 + Pre_("Get"), "", new { });
             }
         }
 
