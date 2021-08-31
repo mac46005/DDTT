9dce8043acfd8e2e48315af569a05d1a0d3e9661
Marco Preciado <mac46005@gmail.com>
2021-08-31 13:04:43 -0500

    Lines Models created

---
+++ SelfEmployedTaxEstimator.ClassLibrary/Models/1040ES_Lines/Line10.cs
@@ -0,0 +1,15 @@
+﻿using SelfEmployedTaxEstimator.ClassLibrary.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfEmployedTaxEstimator.ClassLibrary.Models._1040ES_Lines
+{
+    public class Line10 : IMultiplyByData
+    {
+        public int Id { get; set; }
+        public decimal MultiplyBy { get; set; }
+    }
+}
---
+++ SelfEmployedTaxEstimator.ClassLibrary/Models/Interfaces/IMultiplyByData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfEmployedTaxEstimator.ClassLibrary.Models.Interfaces
+{
+    public interface IMultiplyByData
+    {
+        int Id { get; set; }
+        decimal MultiplyBy { get; set; }
+    }
+}
