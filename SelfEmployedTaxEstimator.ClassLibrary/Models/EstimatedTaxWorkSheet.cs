2bbd567ab94a4a842c4337980a39170c2b602c14
Marco Preciado <mac46005@gmail.com>
2021-09-09 13:40:21 -0500

    EstimatedTaxWK class

--- SelfEmployedTaxEstimator.ClassLibrary/Models/EstimatedTaxWorkSheet.cs
+++ SelfEmployedTaxEstimator.ClassLibrary/Models/EstimatedTaxWorkSheet.cs
@@ -14,5 +14,20 @@ namespace SelfEmployedTaxEstimator.ClassLibrary.Models
         public decimal Line3 { get; set; }
         public decimal Line4 { get; set; }
         public decimal Line5 { get; set; }
+        public decimal Line6 { get; set; }
+        public decimal Line7 { get; set; }
+        public decimal Line8 { get; set; }
+        public decimal Line9 { get; set; }
+        public decimal Line10 { get; set; }
+        public decimal Line11a { get; set; }
+        public decimal Line11b { get; set; }
+        public decimal Line11c { get; set; }
+        public decimal Line12a { get; set; }
+        public decimal Line12b { get; set; }
+        public decimal Line12c { get; set; }
+        public decimal Line13 { get; set; }
+        public decimal Line14a { get; set; }
+        public decimal Line14b { get; set; }
+        public decimal Line15 { get; set; }
     }
 }
