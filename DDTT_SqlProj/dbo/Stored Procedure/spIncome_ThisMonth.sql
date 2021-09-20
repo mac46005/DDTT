1cf8c0f98e492b0a22ff8de7c0bc088b108c05b3
Marco Preciado <mac46005@gmail.com>
2021-09-20 09:41:05 -0500

    spIncome_ThisMonth

---
+++ DDTT_SqlProj/dbo/Stored Procedure/spIncome_ThisMonth.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spIncome_ThisMonth]
+	@Today DATETIME2
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT SUM((BasePay + Tip)) AS TOTALINCOME
+	FROM dbo.Incomes
+	WHERE YEAR(@Today) = YEAR(TimeStamp) AND MONTH(@Today) = MONTH(TimeStamp)
+END
